44def8cc9427bb605404e066185ad287d6c50a3d
Najmiddin <nazarmatovn03@gmail.com>
2023-11-28 02:21:07 +0100

    added knight, fixed prefabs

--- Assets/Scripts/Samurai.cs
+++ Assets/Scripts/Samurai.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class Samurai : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Adjust the speed as needed
+    [SerializeField]
+    private int faction = 0;
+    [SerializeField]
+    public float attackSpeed = 1f;
+    [SerializeField]
+    private float moveSpeed = 5f; // Adjust the speed as needed
     private bool canMove = true; // Flag to determine if the object can move
     private Animator animator; // Reference to the Animator component
     private bool canAttack = true;
     private bool dead = false;
     Lives damageReceiver;
+    [SerializeField]
+    private int damage = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (faction == 2)
+        {
+            RotateObjectBy180Degrees();
+        }
         // Get the Animator component attached to the GameObject
         animator = GetComponent<Animator>();
+    }
+
+    void RotateObjectBy180Degrees()
+    {
+        // Get the current rotation
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+
+        // Set the new rotation by adding 180 degrees to the Y-axis
+        Vector3 newRotation = new Vector3(currentRotation.x, currentRotation.y + 180f, currentRotation.z);
+
+        // Apply the new rotation to the object
+        transform.rotation = Quaternion.Euler(newRotation);
     }
 
     // Update is called once per frame
@@ -36,13 +59,13 @@ public class Samurai : MonoBehaviour
     {
         while (!canMove)
         {
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(attackSpeed);
             animator.SetTrigger("Attack");
             yield return new WaitForSeconds(0.8f);
             if (damageReceiver != null)
             {
                 // Call the GetDamage function on the collided object
-                damageReceiver.GetDamage(10); // You can pass the desired damage value
+                damageReceiver.GetDamage(damage); // You can pass the desired damage value
             }
         }
     }
@@ -52,24 +75,48 @@ public class Samurai : MonoBehaviour
     {
         // Set canMove to false when a collider enters the trigger
         canMove = false;
-        Debug.Log("Triggered");
+
         // Set the "Enemy" parameter to true
         animator.SetBool("Enemy", true);
-        damageReceiver = other.GetComponent<Lives>();
+
+        Samurai samurai = other.GetComponent<Samurai>();
+        if (samurai.getFaction() == faction)
+        {
+            if(samurai.getDamageReceiver() != null)
+            {
+                damageReceiver = samurai.getDamageReceiver();
+            }
+        }
+        else
+        {
+            damageReceiver = other.GetComponent<Lives>();
+        }
         StartCoroutine(Attack());
+
     }
 
     // Called when another collider exits the trigger collider
     void OnTriggerExit2D(Collider2D other)
     {
+        StartCoroutine(TriggerExir(0.5f));
+    }
+
+    IEnumerator TriggerExir(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         if (!dead)
         {
+            damageReceiver = null;
             // Set the "Enemy" parameter to false
             animator.SetBool("Enemy", false);
             // Set canMove to true when a collider exits the trigger
             canMove = true;
         }
-        
+    }
+
+    public Lives getDamageReceiver()
+    {
+        return damageReceiver;
     }
 
     // Function to move the game object towards the right in the X-axis
@@ -89,5 +136,10 @@ public class Samurai : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         Destroy(gameObject);
+    }
+
+    public int getFaction()
+    {
+        return faction;
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject objectToSpawn; // Drag your prefab to this field in the Inspector
     public float spawnCooldown = 2f; // Time limit between spawns
-
+    public float xPos;
     private Button button;
     private bool canSpawn = true;
 
@@ -23,7 +23,7 @@ public class SpawnManager : MonoBehaviour
         {
             StartCoroutine(SpawnCooldown());
             // Specify the position where you want to spawn the object
-            Vector3 spawnPosition = new Vector3(-35f, -10f, 0f);
+            Vector3 spawnPosition = new Vector3(xPos, -10f, 0f);
             // Instantiate a new GameObject based on the prefab at the specified position
             Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
         }
